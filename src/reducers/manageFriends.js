61dbce87813390c900d6145ce83ad7aa950bb9f5
macro6461 <mattcroak718@gmail.com>
2017-10-26 08:20:44 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,15 @@
+let state = { friends: []}
+
 export function manageFriends(state, action){
+  debugger
+  switch(action.type){
+    case "ADD_FRIEND":
+      return Object.assign({}, state, {friends: [...state.friends, action.friend]})
+    case "REMOVE_FRIEND":
+      const removalIndex = state.friends.findIndex(friend => friend.id === action.id)
+      return Object.assign({}, state, {friends: [...state.friends.slice(0, removalIndex),
+          ...state.friends.slice(removalIndex + 1)]})
+    default:
+      return state
+  }
 }
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,12 @@
+let state = {numberOfPresents: 0}
+
 export function managePresents(state, action){
+  switch(action.type){
+    case "INCREASE":
+      return {numberOfPresents: state.numberOfPresents + 1}
+    case "DECREASE":
+      return {numberOfPresents: state.numberOfPresents - 1}
+    default:
+      return state
+  }
 }
